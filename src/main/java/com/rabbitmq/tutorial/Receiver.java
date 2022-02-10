a730ccf7499788ba508b7cc1d3a2bc77401e6d3f
carlos <carlos>
2022-02-10 14:14:38 +0100

    quito desde dev

--- src/main/java/com/rabbitmq/tutorial/Receiver.java
+++ src/main/java/com/rabbitmq/tutorial/Receiver.java
@@ -2,8 +2,9 @@ package com.rabbitmq.tutorial;
 
 public class Receiver {
 
-    public static final String RECEIVE_METHOD_NAME = "receiveMessage";
-
+//    public static final String RECEIVE_METHOD_NAME = "receiveMessage";
+//    public static final String RECEIVE_METHOD_NAME = "receiveMessage";
+//    public static final String RECEIVE_METHOD_NAME = "receiveMessage";
 
     public void receiveMessage(String message) {
         System.out.println("[Receiver] ha recibido el mensaje \"" + message + '"');
